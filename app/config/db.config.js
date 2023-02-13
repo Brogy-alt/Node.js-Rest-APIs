6c57421bbc1096980b772bc1d1da20bf0d3955e7
Brogy-alt <brogangys2@gmail.com>
2023-02-13 16:48:37 +0200

    the pain

---
+++ app/config/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    HOST: "bgaqblpwl0gswnaryoso-mysql.services.clever-cloud.com",
+    USER: "u1iyls4xkwcy27h7",
+    PASSWORD: "38uozKOofDWhNLt5POPp",
+    DB: "bgaqblpwl0gswnaryoso",
+    dialect: "mysql",
+    pool: {
+        max: 15,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+
+};
+
